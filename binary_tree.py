5993a138eff402282a8ee7f55d0385c90cdca203
juliaberk <jroseberk@gmail.com>
2018-05-25 17:35:53 -0700

    Moar Binary Tree

--- binary_tree.py
+++ binary_tree.py
@@ -33,3 +33,40 @@ def is_BST(node):
     # helper function, one level of nesting is cool
     def traverse (node):
         #append to local variable
+
+        
+#   ###################
+
+class BinaryNode(object):   # ...
+    def is_balanced(self):
+        """Is the tree at this node balanced?"""
+
+        # START SOLUTION
+
+        def _num_descendants(node):
+            """Returns number of descendants or None if already imbalanced."""
+
+            if not node:
+                # Our base case: we're not a real node (child of a leaf)
+                return 0
+
+            # Get descendants on left; if "None", already imbalanced---bail out
+            left = _num_descendants(node.left)
+
+            if left is None:
+                return None
+
+            # Get descendants on right; if "None", already imbalanced---bail out
+            right = _num_descendants(node.right)
+
+            if right is None:
+                return None
+
+            if abs(left - right) > 1:
+                # Heights vary by more than 1 -- imbalanced!
+                return None
+
+            # Height of this node is height of our deepest descendant + ourselves
+            return max(left, right) + 1
+
+        return _num_descendants(self) is not None
