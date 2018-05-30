18718e5b7c5b4dfd1a982e45724d288d3ada262e
juliaberk <jroseberk@gmail.com>
2018-05-29 21:56:07 -0700

    Balanced? Binary Tree

---
+++ Balanced_B_Tree.py
@@ -0,0 +1,16 @@
+# Is the binary tree balanced? 
+
+class BinaryTreeNode(object):
+
+    def __init__(self, value):
+        self.value = value
+        self.left  = None
+        self.right = None
+
+    def insert_left(self, value):
+        self.left = BinaryTreeNode(value)
+        return self.left
+
+    def insert_right(self, value):
+        self.right = BinaryTreeNode(value)
+        return self.right
